d0399925fc892d28fede8683c9aa5d710182f666
Dmitriy Chereukho <dachereukho@edu.hse.ru>
2024-05-21 20:06:41 +0300

    Add base registration

--- src/main/kotlin/ru/hse/virtual/psychologist/backend/services/AuthUserDetailsService.kt
+++ src/main/kotlin/ru/hse/virtual/psychologist/backend/services/AuthUserDetailsService.kt
@@ -1,16 +1,17 @@
 package ru.hse.virtual.psychologist.backend.services
 
+import org.springframework.http.HttpStatus
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.security.core.userdetails.User
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class AuthUserDetailsService(private val userService: UserService) : UserDetailsService {
     override fun loadUserByUsername(username: String): UserDetails {
         return userService.findByEmail(username)?.mapToUserDetails()
-            ?: throw UsernameNotFoundException("User not found")
+            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
     }
 
     private fun ru.hse.virtual.psychologist.backend.data.entities.User.mapToUserDetails(): UserDetails {
