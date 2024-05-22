34101a8946f190321b67fa6c29922182b9b946cf
Dmitriy Chereukho <dachereukho@edu.hse.ru>
2024-05-22 19:15:34 +0300

    email and phone num uniqueness check

--- src/main/kotlin/ru/hse/virtual/psychologist/backend/controllers/UserController.kt
+++ src/main/kotlin/ru/hse/virtual/psychologist/backend/controllers/UserController.kt
@@ -2,10 +2,7 @@ package ru.hse.virtual.psychologist.backend.controllers
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import ru.hse.virtual.psychologist.backend.dtos.UserDto
 import ru.hse.virtual.psychologist.backend.mappers.UserDtoToUserEntityMapper
