3c8baa6bbb64d1177f7183a676c5b7e9a7d29cc2
Dmitriy Chereukho <dachereukho@edu.hse.ru>
2024-05-22 18:29:05 +0300

    email and phone num uniqueness check

--- src/main/kotlin/ru/hse/virtual/psychologist/backend/controllers/UserController.kt
+++ src/main/kotlin/ru/hse/virtual/psychologist/backend/controllers/UserController.kt
@@ -1,16 +1,30 @@
 package ru.hse.virtual.psychologist.backend.controllers
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import ru.hse.virtual.psychologist.backend.dtos.UserDto
 import ru.hse.virtual.psychologist.backend.mappers.UserDtoToUserEntityMapper
 import ru.hse.virtual.psychologist.backend.services.UserService
 
 @RestController
-class UserController(private val userService: UserService, private val userDtoToUserEntityMapper: UserDtoToUserEntityMapper) {
+class UserController(
+    private val userService: UserService,
+    private val userDtoToUserEntityMapper: UserDtoToUserEntityMapper
+) {
     @PostMapping("/signup")
-    fun signup(@RequestBody user: UserDto) : UserDto {
-        return userDtoToUserEntityMapper.map(userService.createUser(userDtoToUserEntityMapper.map(user)))
+    @ResponseStatus(HttpStatus.CREATED)
+    fun signup(@RequestBody user: UserDto): ResponseEntity<UserDto> {
+        try {
+            val createdUser = userService.createUser(userDtoToUserEntityMapper.map(user))
+            return ResponseEntity.ok(userDtoToUserEntityMapper.map(createdUser))
+        }
+        catch (e: ResponseStatusException) {
+            return ResponseEntity.status(e.reason!!.toInt()).build()
+        }
     }
 }
