28a168efc80ef3aa73c5a86bbca0a4013925293a
Dmitriy Chereukho <dachereukho@edu.hse.ru>
2024-05-24 20:25:02 +0300

    refactor

--- src/main/kotlin/ru/hse/virtual/psychologist/backend/controllers/UserController.kt
+++ src/main/kotlin/ru/hse/virtual/psychologist/backend/controllers/UserController.kt
@@ -1,6 +1,7 @@
 package ru.hse.virtual.psychologist.backend.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.hse.virtual.psychologist.backend.dtos.UserInfoDto
