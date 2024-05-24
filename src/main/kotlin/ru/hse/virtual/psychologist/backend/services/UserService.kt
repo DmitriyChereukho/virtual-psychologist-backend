c93cdde8fe8c176fcba614672c3e54c2a1c68c69
te0r1ty <max.polyakov@inbox.ru>
2024-05-24 20:33:31 +0300

    User info and controller fixes

--- src/main/kotlin/ru/hse/virtual/psychologist/backend/services/UserService.kt
+++ src/main/kotlin/ru/hse/virtual/psychologist/backend/services/UserService.kt
@@ -1,14 +1,21 @@
 package ru.hse.virtual.psychologist.backend.services
 
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import ru.hse.virtual.psychologist.backend.data.entities.User
 import ru.hse.virtual.psychologist.backend.data.repositories.UserRepository
+import ru.hse.virtual.psychologist.backend.dtos.UserInfoDto
 import ru.hse.virtual.psychologist.backend.exceptions.email.EmailExistsException
 import ru.hse.virtual.psychologist.backend.exceptions.phone.PhoneExistsException
+import ru.hse.virtual.psychologist.backend.mappers.UserEntityToUserInfoDtoImpl
 
 @Service
-class UserService(private val userRepository: UserRepository, private val encoder: PasswordEncoder) {
+class UserService(
+    private val userRepository: UserRepository,
+    private val encoder: PasswordEncoder,
+    private val userEntityToUserInfoDto: UserEntityToUserInfoDtoImpl,
+) {
     fun findByEmail(email: String): User? {
         return userRepository.findAll().find { it.email == email }
     }
@@ -23,5 +30,10 @@ class UserService(private val userRepository: UserRepository, private val encode
         if(findByPhoneNum(user.phoneNum) != null) throw PhoneExistsException()
 
         return userRepository.save(user.copy(password = encoder.encode(user.password)))
+    }
+
+    fun getInfo() : UserInfoDto {
+        return userEntityToUserInfoDto.map(
+            findByEmail(SecurityContextHolder.getContext().authentication.name))
     }
 }
