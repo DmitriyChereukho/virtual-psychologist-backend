560b02a1e899cfb4608f5b867b73b5b32a737495
Dmitriy Chereukho <dachereukho@edu.hse.ru>
2024-05-24 12:49:55 +0300

    added cross origin config

--- src/main/kotlin/ru/hse/virtual/psychologist/backend/controllers/TestCaseController.kt
+++ src/main/kotlin/ru/hse/virtual/psychologist/backend/controllers/TestCaseController.kt
@@ -1,7 +1,6 @@
 package ru.hse.virtual.psychologist.backend.controllers
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
